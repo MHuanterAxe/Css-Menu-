0a0d5540e9ee5fa617a5577830ee2d8da4d9b72c
MHuanterAxe <maks.shkm@gmail.com>
2019-09-24 23:19:24 +0300

    v0.0.5

--- main.js
+++ main.js
@@ -6,20 +6,23 @@ $(document).ready(function () {
     main.mousedown(function () { 
         if($('.panel').hasClass('hiden')){
             let t = anime.timeline({
-                easing:'linear',
-                duration:150
+                easing: 'cubicBezier(.5, .05, .1, .3)',
+                duration:200
             })
             t.add({
                 targets:'#p4',
                 top:330,
+                translateZ: 5
             })
             t.add({
                 targets:'#p3',
                 top:220,
+                translateZ: 4
             })
             t.add({
                 targets: '#p2',
                 top: 110,
+                translateZ: 3
             })
             t.add({
                 targets: ['#p1',inf],
@@ -30,8 +33,8 @@ $(document).ready(function () {
             
         }else{
             let tl = anime.timeline({
-                easing:'linear',
-                duration:150
+                easing: 'cubicBezier(.5, .05, .1, .3)',
+                duration:200
             })
             tl.add({
                 targets: ['#p1',inf],
@@ -40,14 +43,17 @@ $(document).ready(function () {
             tl.add({
                 targets: '#p2',
                 top: 20,
+                translateZ: -3
             })
             tl.add({
                 targets:'#p3',
                 top:40,
+                translateZ: -4
             })
             tl.add({
                 targets:'#p4',
                 top:60,
+                translateZ: -5
 
             })
     
