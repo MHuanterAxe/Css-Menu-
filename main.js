a904df25297d7b6117e008651b003968ebf49e08
Shkurin Maxim <maks.shkm@gmail.com>
2019-09-24 19:28:53 +0300

    AnimationV1

--- main.js
+++ main.js
@@ -1,44 +1,23 @@
 
 $(document).ready(function () {
-    let main = $('.main');
-    let inf = $('.information-panel').children('.lesson');
+    let main = $('#p1');
+    let inf = $('.information-panel').children('.panel');
     console.log(inf);
     main.mousedown(function () { 
-        
-        
-        if(!inf.hasClass('active')){
-            let informpabel = anime({
-                targets: '.information-panel',
-                height: 0,
-                duration: 1000,
-                easing: 'easeInQuad'
-            })
-            let blocks = anime({
-                targets: '.lesson',
-                translateY: -50,
-                translateZ: -10,
-                opcity: 0,
-                duration: 1000,
-                easing: 'easeInQuad'
-            })
-        }else{
-            informpabel = anime({
-                targets: '.information-panel',
-                height: 150,
-                duration: 1000,
-                easing: 'easeInQuad'
-            })
-            blocks = anime({
-                targets: '.lesson',
-                translateY: 50,
-                translateZ: 10,
-                opcity: 1,
-                duration: 1000,
-                easing: 'easeInQuad'
-    
-            })
-        }
-        inf.toggleClass('active');
-        
+        anime ({
+            targets: '.panel',
+            translateZ:-10,
+            top: 0,
+            easing: 'linear'
+        })  
+        anime({
+            targets: '#p1',
+            translateZ: 10
+        }) 
+        anime({
+            targets: ['#p2', '#p3'],
+            opacity: 0,
+            duration: 3000
+        })
     });
 });
